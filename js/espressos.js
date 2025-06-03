a95700c78755a7000e4f07bf8a8535d49933dd4c
Player_MINEPIG <zpig666@163.com>
2025-06-03 19:51:12 +1000

    Implement price change handler

--- js/espressos.js
+++ js/espressos.js
@@ -18,6 +18,9 @@ document.addEventListener('DOMContentLoaded', () => {
         });
     });
 
+    // Add to cart availability
+    let addToCartAvailable = false;
+
     // Add size option click handler
     const sizes = document.querySelectorAll('.size');
     sizes.forEach(size => {
@@ -30,108 +33,153 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     // Add quantity click handler
-    const quantityInputs = document.querySelectorAll('.quantity');
-    quantityInputs.forEach(input => {
-        input.querySelector('.minus-icon').addEventListener('click', () => {
-            onMinusIconClick(input);
+    const quantityContainers = document.querySelectorAll('.quantity');
+    quantityContainers.forEach(container => {
+        container.querySelector('.minus-icon').addEventListener('click', () => {
+            onMinusIconClick(container);
         });
-        input.querySelector('.plus-icon').addEventListener('click', () => {
-            onPlusIconClick(input);
+        container.querySelector('.plus-icon').addEventListener('click', () => {
+            onPlusIconClick(container);
+        });
+        container.querySelector('.quantity-number').addEventListener('change', () => {
+            onQuantityChange(container);
         });
     });
-});
 
-// Expand product card animation
-function expandProductCard(targetCard) {
-    if (targetCard.classList.contains('expanded')) return;
-    const productCards = document.querySelectorAll('.product-card');
-    productCards.forEach(card => {
-        fadeOut(card);
+    // Expand product card animation
+    function expandProductCard(targetCard) {
+        if (targetCard.classList.contains('expanded')) return;
+        const productCards = document.querySelectorAll('.product-card');
+        productCards.forEach(card => {
+            fadeOut(card);
 
-        card.removeEventListener('transitionend', onTransitionEnd);
+            card.removeEventListener('transitionend', onTransitionEnd);
 
-        card.addEventListener('transitionend', onTransitionEnd);
-    });
+            card.addEventListener('transitionend', onTransitionEnd);
+        });
 
-    function onTransitionEnd(e) {
-        // opacity
-        if (e.propertyName === 'opacity') {
-            onFadeEnd(e);
-            return;
-        };
+        function onTransitionEnd(e) {
+            // opacity
+            if (e.propertyName === 'opacity') {
+                onFadeEnd(e);
+                return;
+            };
 
-        // flex
-        if (e.propertyName === 'flex-grow') {
-            onFlexTransitionEnd(e);
-            return;
-        };
-    }
+            // flex
+            if (e.propertyName === 'flex-grow') {
+                onFlexTransitionEnd(e);
+                return;
+            };
+        }
 
-    function onFadeEnd(e) {
-        // check if fade out
-        const opacity = window.getComputedStyle(e.currentTarget.querySelector('.product-info')).opacity;
-        if (opacity === '0') {
+        function onFadeEnd(e) {
+            // check if fade out
+            const opacity = window.getComputedStyle(e.currentTarget.querySelector('.product-info')).opacity;
+            if (opacity === '0') {
+                const detailPs = e.currentTarget.querySelectorAll('.product-detail p');
+                detailPs.forEach(p => {
+                    p.style.display = 'none';
+                });
+
+                if (e.currentTarget === targetCard) {
+                    targetCard.classList.add('expanded');
+                } else {
+                    e.currentTarget.classList.remove('expanded');
+                }
+            }
+        }
+
+        function onFlexTransitionEnd(e) {
             const detailPs = e.currentTarget.querySelectorAll('.product-detail p');
             detailPs.forEach(p => {
-                p.style.display = 'none';
+                p.style.display = 'flex';
             });
-
-            if (e.currentTarget === targetCard) {
-                targetCard.classList.add('expanded');
-            } else {
-                e.currentTarget.classList.remove('expanded');
-            }
+            fadeIn(e.currentTarget);
         }
     }
 
-    function onFlexTransitionEnd(e) {
-        const detailPs = e.currentTarget.querySelectorAll('.product-detail p');
-        detailPs.forEach(p => {
-            p.style.display = 'flex';
-        });
-        fadeIn(e.currentTarget);
+    function fadeOut(card) {
+        card.classList.remove('fade-in');
+        card.classList.add('fade-out');
     }
-}
 
-function fadeOut(card) {
-    card.classList.remove('fade-in');
-    card.classList.add('fade-out');
-}
-
-function fadeIn(card) {
-    card.classList.remove('fade-out');
-    card.classList.add('fade-in');
-}
-
-// Size option click
-function onSizeOptionClick(targetOption, options, size) {
-    const addToCartBtn = size.closest('.product-info').querySelector('.add-to-cart');
-    
-    if (targetOption.classList.contains('selected')) {
-        options.forEach(option => {
-            option.classList.remove('selected');
-        });
-        addToCartBtn.classList.remove('available');
-    } else {
-        options.forEach(option => {
-            option.classList.remove('selected');
-        });
-        targetOption.classList.add('selected');
-        addToCartBtn.classList.add('available');
+    function fadeIn(card) {
+        card.classList.remove('fade-out');
+        card.classList.add('fade-in');
     }
-}
 
-// Quantity click
-function onMinusIconClick(targetInput) {
-    const quantityNumber = targetInput.querySelector('.quantity-number');
-    const quantity = parseInt(quantityNumber.value);
-    quantityNumber.value = Math.max(1, quantity - 1);
-}
+    // Size option click
+    function onSizeOptionClick(targetOption, options, size) {
+        const productInfo = size.closest('.product-info');
+        const addToCartBtn = productInfo.querySelector('.add-to-cart');
 
-function onPlusIconClick(targetInput) {
-    const quantityNumber = targetInput.querySelector('.quantity-number');
-    const quantity = parseInt(quantityNumber.value);
-    quantityNumber.value = Math.min(99, quantity + 1);
-}
+        if (targetOption.classList.contains('selected')) {
+            options.forEach(option => {
+                option.classList.remove('selected');
+            });
+            addToCartAvailable = false;
+            onPriceChange(productInfo);
+            addToCartBtn.classList.remove('available');
+        } else {
+            options.forEach(option => {
+                option.classList.remove('selected');
+            });
+            targetOption.classList.add('selected');
+            addToCartAvailable = true;
+            onPriceChange(productInfo);
+            addToCartBtn.classList.add('available');
+        }
+    }
+
+    // Quantity click
+    function onMinusIconClick(targetContainer) {
+        const quantityNumber = targetContainer.querySelector('.quantity-number');
+        const quantity = parseInt(quantityNumber.value);
+        quantityNumber.value = Math.max(1, quantity - 1);
+        onPriceChange(targetContainer.closest('.product-info'));
+    }
+
+    function onPlusIconClick(targetContainer) {
+        const quantityNumber = targetContainer.querySelector('.quantity-number');
+        const quantity = parseInt(quantityNumber.value);
+        quantityNumber.value = Math.min(99, quantity + 1);
+        onPriceChange(targetContainer.closest('.product-info'));
+    }
+
+    // Quantity change
+    function onQuantityChange(targetContainer) {
+        const quantityNumber = targetContainer.querySelector('.quantity-number');
+        const quantity = parseInt(quantityNumber.value);
+        if (quantity < 1) {
+            quantityNumber.value = 1;
+        }
+        if (quantity > 99) {
+            quantityNumber.value = 99;
+        }
+        const productInfo = targetContainer.closest('.product-info');
+        onPriceChange(productInfo);
+    }
+
+    // Price Change
+    function onPriceChange(productInfo) {
+        const priceNumber = productInfo.querySelector('.price .price-number');
+        if (addToCartAvailable) {
+            productInfo.querySelector('.price span').style.display = 'none';
+            priceNumber.style.display = 'flex';
+        }
+        else {
+            productInfo.querySelector('.price span').style.display = 'flex';
+        }
+        const defaultPricePerUnit = 22.00;
+        const selectedSizeOption = productInfo.querySelector('.size-option.selected');
+        if (selectedSizeOption === null) {
+            priceNumber.textContent = `A$${defaultPricePerUnit * parseInt(productInfo.querySelector('.quantity .quantity-number').value)}`;
+        } else {
+            priceNumber.textContent = `A$${selectedSizeOption.dataset.price * parseInt(productInfo.querySelector('.quantity .quantity-number').value)}`;
+        }
+    }
+
+});
+
 
 
