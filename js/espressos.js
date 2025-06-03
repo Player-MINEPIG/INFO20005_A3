6b2e4181c17c87eaca9fbb71d5e9d866f97f23b1
Player_MINEPIG <zpig666@163.com>
2025-06-03 16:56:12 +1000

    Card expand animation, display bug has not been fixed yet

--- js/espressos.js
+++ js/espressos.js
@@ -12,12 +12,64 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
-function expandProductCard(card) {
+function expandProductCard(targetCard) {
+    if (targetCard.classList.contains('expanded')) return;
     const productCards = document.querySelectorAll('.product-card');
     productCards.forEach(card => {
-        card.classList.remove('expanded');
+        // card.classList.remove('expanded');
+        fadeOut(card);
+
+        card.removeEventListener('transitionend', onTransitionEnd);
+
+        card.addEventListener('transitionend', onTransitionEnd);
     });
-    card.classList.add('expanded');
+
+    targetCard.classList.add('expanded');
+
+    function onTransitionEnd(e) {
+        console.log(e.propertyName);
+        console.log(e.currentTarget);
+        console.log("transitionend");
+        
+
+        // opacity
+        if (e.propertyName === 'opacity') {
+            onFadeOutEnd(e);
+            console.log("fadeoutend");
+            console.log("----------");
+            return;
+        };
+
+        // flex
+        if (e.propertyName === 'flex-grow') {
+            onFlexTransitionEnd(e);
+            console.log("flextransitionend");
+            console.log("----------");
+            return;
+        };
+    }
+
+    function onFadeOutEnd(e) {
+        if (e.currentTarget === targetCard) {
+            targetCard.classList.add('expanded');
+        } else {
+            e.currentTarget.classList.remove('expanded');
+        }
+    }
+
+    function onFlexTransitionEnd(e) {
+        fadeIn(e.currentTarget);
+    }
+}
+
+function fadeOut(card) {
+    card.classList.remove('fade-in');
+    card.classList.add('fade-out');
+}
+
+function fadeIn(card) {
+    card.classList.remove('fade-out');
+    card.classList.add('fade-in');
 }
 
 // Index logo navigation is handled by the sidebar module
