ce298b43b59497ce4e513786e87cc0478585c06c
Player_MINEPIG <zpig666@163.com>
2025-06-04 20:25:48 +1000

    Basic style for products in cart drawer

--- js/insertCartDrawer.js
+++ js/insertCartDrawer.js
@@ -1,4 +1,5 @@
 import { initSidebar } from './insertSidebar.js';
+import { getCart } from './cartOperation.js';
 
 export function initCartDrawer() {
     // Load different cart drawer HTML for different pages
@@ -36,10 +37,50 @@ export function showCartDrawer() {
     document.querySelector('.cart-drawer').classList.add('active');
     document.querySelector('.cart-overlay').classList.add('active');
     document.dispatchEvent(new Event('cartDrawerShown'));
+    updateCartProducts();
 }
 
 export function hideCartDrawer() {
     document.querySelector('.cart-drawer').classList.remove('active');
     document.querySelector('.cart-overlay').classList.remove('active');
     document.dispatchEvent(new Event('cartDrawerHidden'));
+}
+
+let productNameImgMap = {
+    'Colombia - Hulia Decaf De Cana': '../assets/images/hulia-cut.jpg',
+    'Colombia - China Alta': '../assets/images/china-cut.jpg'
+}
+
+let productNameBackgroundMap = {
+    'Colombia - Hulia Decaf De Cana': 'linear-gradient(180deg, #E1BB67 0%, #9E844B 150%)',
+    'Colombia - China Alta': 'linear-gradient(180deg, #A65A47 0%, #894534 100%)'
+}
+
+function updateCartProducts() {
+    const cartProducts = document.querySelector('.cart-products');
+    cartProducts.innerHTML = '';
+    const cart = getCart();
+    console.log(cart);
+    cart.forEach(product => {
+        console.log(product.name);
+        console.log(productNameImgMap[product.name]);
+        cartProducts.innerHTML += `
+            <div class="cart-product" style="background: ${productNameBackgroundMap[product.name]}">
+                <h3>${product.name}</h3>
+                <span class="cart-product-size">
+                    <span>Size: </span>
+                    <span class="cart-product-size-option">${product.size}</span>
+                </span>
+                <img class="cart-product-img" src="${productNameImgMap[product.name]}" alt="${product.name}">
+                <span class="cart-product-price">
+                    <span class="cart-product-price-number">A$${product.pricePerUnit * product.quantity}</span>
+                </span>
+                <span class="cart-product-quantity">
+                    <img class="minus-icon" src="../assets/images/triangle.svg" alt="Minus">
+                    <input type="number" class="quantity-number" value="${product.quantity}" min="1" max="99">
+                    <img class="plus-icon" src="../assets/images/triangle.svg" alt="Plus">
+                </span>
+            </div>
+        `;
+    });
 }
